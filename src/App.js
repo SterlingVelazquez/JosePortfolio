1553edae069933a59e4dfa93dcd02a6892ce3393
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2021-03-27 14:50:54 -0400

    Added Image and Animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/main.css';
+import particles from './components/ParticleImage.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+try {
+  particles()
+} catch {
+  // Error
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div className="introOverlay"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
